d6588fe721ea78fa4f4187c97a721b6d9d377c68
POOJA PATEL P <poojapatelp294@gmail.com>
2024-08-20 12:50:47 +0530

    java programs

---
+++ selenium1/India.java
@@ -0,0 +1,11 @@
+package selenium1;
+
+public class India {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ selenium1/Launch.java
@@ -0,0 +1,23 @@
+package selenium1;
+//Launch all the web browsers available on your system through selenium code
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch
+{
+	public static void main(String[] args) 
+	{
+		ChromeDriver driver= new ChromeDriver();
+		
+		//FirefoxDriver f=new FirefoxDriver();
+		
+		EdgeDriver e=new EdgeDriver();
+		
+		
+		
+
+	}
+}
